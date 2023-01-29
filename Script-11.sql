ede318002b7b0ee0fa53e84c6ce0376b60d7edbe
Tatiana <83293693+TatianaMaslova@users.noreply.github.com>
2023-01-29 12:18:12 +0200

    Add files via upload
    Examples of my SQL queries

---
+++ Script-11.sql
@@ -0,0 +1,22 @@
+-- selecting saved tests/panels in the order with their setting
+SELECT 'Test' AS 'Test/Panel', t.LABNO, CONCAT(md.TEST_TYPE, ' - ', md.MNEMONIC, ' - ', md.LABEL) AS 'Test Name', 
+t.CUSTOMER_ORDER_ID, t.PRIORITY 'Priority Status',
+	CASE 
+		when t.REFLEXABLE = '0' then 'Unchecked'
+		ELSE 'Checked'
+	END as 'Reflex Status'
+FROM TEST_MST AS md
+	INNER JOIN ORDER_TEST AS t ON t.MD_TEST_ID = md.ID AND t.STATUS =1 AND t.PANEL_ID IS NULL 
+	INNER JOIN ORDERS o ON o.ID =t.ORDER_ID 
+WHERE o.ACC_ID = 'AC1258168'
+UNION 
+SELECT 'Panel', p.LABNO, CONCAT(mp.TEST_TYPE, ' - ', mp.MNEMONIC,' - ', mp.LABEL), p.CUSTOMER_ORDER_ID, 
+p.PRIORITY, 
+	CASE 
+		when p.REFLEXABLE = '0' then 'Unchecked'
+		ELSE 'Checked'
+	END  
+FROM PANEL_MST mp 
+	INNER JOIN ORDER_PANEL p ON p.MD_PANEL_ID = mp.ID AND p.STATUS =1
+	INNER JOIN ORDERS o ON o.ID = p.ORDER_ID 
+WHERE o.ACC_ID ='AC1258168'
